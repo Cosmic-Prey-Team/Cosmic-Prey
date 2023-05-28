8a717ca15d49ce13022cde66ff15538805697ed6
JoshAltshuler <Josh.Altshuler1@gmail.com>
2023-05-27 20:57:32 -0500

    Spawn Asteroids
    Made a script to spawn asteroids across the map, making sure no asteroids are within a set distance of each other

---
+++ Assets/Game Assets/Scripts/SpawnAsteroids.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAsteroids : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _asteroid;
+
+    [SerializeField]
+    private float _minDistanceApart = 50;
+
+    [SerializeField]
+    private int _numAsteroidsToSpawn = 10;
+
+    [SerializeField]
+    private float _mapSize = 500;
+
+
+    private Vector3 FindNewPosition()
+    {
+        Vector3 Position;
+        Collider[] _collision;
+
+        do
+        {
+            //generates a random position within the map
+            Position = new Vector3(Random.Range(-_mapSize, _mapSize), Random.Range(-_mapSize, _mapSize), Random.Range(-_mapSize, _mapSize));
+
+            //checks if anything exists within a range of _minDistanceApart
+            //this means no asteroids will spawn within a set distance from the ship and whale's spawning position
+            _collision = Physics.OverlapSphere(Position, _minDistanceApart);
+
+            //repeat until we find a position far enough from everything
+        } while (_collision.Length > 0);
+
+        //if it is far enough from everything, return the generated position
+        return Position;
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < _numAsteroidsToSpawn; i++)
+        {
+            Vector3 Position = FindNewPosition();
+            Instantiate(_asteroid, Position, transform.rotation);
+        }
+    }
+}
