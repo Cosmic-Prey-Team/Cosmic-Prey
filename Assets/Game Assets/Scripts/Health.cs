6443b5b944549014bad2add86d0123144de03667
aregaIado <boxcatdev@gmail.com>
2023-06-01 11:57:15 -0500

    added _maxHealth and some methods for the health bar
    - added GetHealth() and GetHealthPercent()
    - added if statements to prevent health value going past min/max limits
    - trigger Die() if health at or below 0

--- Assets/Game Assets/Scripts/Health.cs
+++ Assets/Game Assets/Scripts/Health.cs
@@ -5,31 +5,50 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private int _health;
+    [SerializeField] private int _maxHealth;
+    private int _health;
     
     // events for future use
     public event Action OnHealthChanged;
     public event Action OnDeath;
 
+    private void Awake()
+    {
+        //sets the current health to max
+        _health = _maxHealth;
+    }
 
-    
+    //returns health value
+    public int GetHealth()
+    {
+        return _health;
+    }
+    //returns health as a percent of maxHealth
+    public float GetHealthPercent()
+    {
+        return (float)_health / _maxHealth;
+    }
+
     public void Damage(int damage)
     {
         _health -= damage;
+        if (_health < 0) _health = 0;
         OnHealthChanged?.Invoke();
+
+        if (_health == 0) Die();
     }
 
     public void Heal(int amount)
     {
         _health += amount;
+        if (_health > _maxHealth) _health = _maxHealth;
         OnHealthChanged?.Invoke();
     }
 
     public void Die()
     {
-        Destroy(gameObject);
         OnDeath?.Invoke();
-     
+        Destroy(gameObject);
     }
 
 
