966b38a273c724cc4dfd901a066adb018f138ae5
JoshAltshuler <Josh.Altshuler1@gmail.com>
2023-06-15 14:33:50 -0500

    Move Ship Vertically
    Able to move the ship up and down

--- Assets/Game Assets/Scripts/ShipController.cs
+++ Assets/Game Assets/Scripts/ShipController.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    //private bool _inShipState;
-
     [SerializeField] private GameObject _waypoint;
+    [SerializeField] private GameObject _verticalWaypoint;
     [SerializeField] private PlayerState _ps;
     [SerializeField] public Vector3 velocity;
 
     [SerializeField] private float _maxSpeed = 1f;
     [SerializeField] private float _accelerationDelay;
-    private float _speed = 0f;
-    private float _accelerate;
+    private float _speed, _vSpeed = 0f;
+    private float _accelerate, _vAccelerate;
 
     [SerializeField] private InputHandler _input;
-
-    private IEnumerator _accelerator;
     
 
     // Update is called once per frame
@@ -26,8 +23,8 @@ public class ShipController : MonoBehaviour
 
         if (_waypoint)
         {
-            Debug.Log(_speed);
             transform.position = Vector3.MoveTowards(transform.position, _waypoint.transform.position, _speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, _verticalWaypoint.transform.position, _vSpeed * Time.deltaTime);
             
             //NOTE: Change Velocity.z to apply to Velocity.x as well. How would we set the Vector3 Velocity to the same as the ships?
             velocity.z = _speed * Time.deltaTime;
@@ -60,7 +57,7 @@ public class ShipController : MonoBehaviour
                     _accelerate = Time.time + _accelerationDelay;
                 }
             }
-            //if the player is not moving the ship forwards
+            //if the player is trying to stop moving
             else if (_input.move.y < 0)
             {
                 if(_speed > 0 && Time.time > _accelerate)
@@ -73,7 +70,62 @@ public class ShipController : MonoBehaviour
                     _accelerate = Time.time + _accelerationDelay;
                 }
             }
-                
+            
+            //if the player is trying to move up
+            if (_input.vMove > 0)
+            {
+                if(_vSpeed < _maxSpeed && Time.time > _vAccelerate)
+                {
+                    if (_vSpeed >= 0)
+                        _vSpeed = (_vSpeed * 2) + .05f;
+                    else
+                        _vSpeed = (_vSpeed / 2) + .05f;
+
+                    if (_vSpeed > _maxSpeed)
+                        _vSpeed = _maxSpeed;
+
+                    _vAccelerate = Time.time + _accelerationDelay;
+                }
+            }
+            //if the player is trying to move down
+            else if (_input.vMove < 0)
+            {
+                if (_vSpeed > -_maxSpeed && Time.time > _vAccelerate)
+                {
+                    if (_vSpeed <= 0)
+                        _vSpeed = (_vSpeed * 2) - .05f;
+                    else
+                        _vSpeed = (_vSpeed / 2) - .05f;
+
+                    if (_vSpeed < -_maxSpeed)
+                        _vSpeed = -_maxSpeed;
+
+                    _vAccelerate = Time.time + _accelerationDelay;
+                }
+            }
+        }
+
+       //bring vertical movement to 0 if there is no input or player is no longer controlling ship
+       if(_input.vMove == 0)
+        {
+            if (_vSpeed > 0 && Time.time > _vAccelerate)
+            {
+                _vSpeed = (_vSpeed / 2) - .05f;
+
+                if (_vSpeed < 0)
+                    _vSpeed = 0;
+
+                _vAccelerate = Time.time + _accelerationDelay;
+            }
+            else if(_vSpeed < 0 && Time.time > _vAccelerate)
+            {
+                _vSpeed = (_vSpeed / 2) + .05f;
+
+                if (_vSpeed > 0)
+                    _vSpeed = 0;
+
+                _vAccelerate = Time.time + _accelerationDelay;
+            }
         }
     }
 }
