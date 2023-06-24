9bac3fe0dd172136b1fe47ede7c55c7fab98d2b2
aregaIado <boxcatdev@gmail.com>
2023-06-24 00:46:03 -0500

    updated input handler script and test processor script
    - input handler SetCursorState() made public
    - tested processor menu opening with new interactions

--- Assets/Game Assets/Scripts/Interactions/InputHandler.cs
+++ Assets/Game Assets/Scripts/Interactions/InputHandler.cs
@@ -75,7 +75,7 @@ public class InputHandler : MonoBehaviour
 		SetCursorState(cursorLocked);
 	}
 
-	private void SetCursorState(bool newState)
+    public void SetCursorState(bool newState)
 	{
 		Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
 	}
--- Assets/Game Assets/Scripts/Interactions/ProcessorInteractable.cs
+++ Assets/Game Assets/Scripts/Interactions/ProcessorInteractable.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class ProcessorInteractable : MonoBehaviour, IInteractable
 {
+    [Header("IInteractable variables")]
     [SerializeField] string _interactText;
     [SerializeField] bool _isInteracting = false;
 
+    [Header("Processor variables")]
+    [SerializeField] GameObject _processorMenuUI;
+
+    #region IInteractable Methods
     public string GetInteractText()
     {
         return _interactText;
@@ -19,10 +24,45 @@ public class ProcessorInteractable : MonoBehaviour, IInteractable
 
     public void TriggerInteraction(Transform transform, bool isInteracting)
     {
-        if (_isInteracting != isInteracting) _isInteracting = isInteracting;
+        if (_isInteracting != isInteracting)
+        {
+            _isInteracting = isInteracting;
+            ToggleProcessorMenu(isInteracting);
+        }
+
     }
     public void LeaveInteraction()
     {
-        if (_isInteracting == true) _isInteracting = false;
+        if (_isInteracting == true)
+        {
+            _isInteracting = false;
+            ToggleProcessorMenu(false);
+        }
     }
+    #endregion
+
+    #region Monobehavior
+    private void Start()
+    {
+        _processorMenuUI.SetActive(false);
+    }
+    #endregion
+
+    #region Custom Methods
+    private void ToggleProcessorMenu(bool menuActive)
+    {
+        Debug.Log("Processor menu: " + menuActive);
+        if(_processorMenuUI != null)
+        {
+            if (menuActive)
+            {
+                _processorMenuUI.SetActive(true);
+            }
+            else
+            {
+                _processorMenuUI.SetActive(false);
+            }
+        }
+    }
+    #endregion
 }
