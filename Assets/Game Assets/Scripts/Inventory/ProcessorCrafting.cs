5ed5ece348f2cd05c8e94ea7a8edeab29dd0f58b
Xernacus <a10rec@myfastmail.com>
2023-07-19 10:32:22 -0500

    Inventory Updates
    Inventory slots now created based on location of placeholder gameobjects that can be edited in inspector. Crafter now uses queue to craft specific amount of items. Fixed bugs of items deleting themselves when dragged to same inventory and item count not being preserved through moves.

--- Assets/Game Assets/Scripts/Inventory/ProcessorCrafting.cs
+++ Assets/Game Assets/Scripts/Inventory/ProcessorCrafting.cs
@@ -8,16 +8,18 @@ using UnityEngine.UI;
 
 public class ProcessorCrafting : MonoBehaviour
 {
-    public int materialToCraft = -1;
-    public float craftInterval = 10f;
-    public float craftTimer = 10f;
+    public float craftInterval = 5f;
+    public float craftTimer = 5f;
     public GameObject canvas;
     public Slider progressBar;
     public InventoryItemSO Ammo;
     public InventoryItemSO Panel;
     public InventoryItemSO MachinePart;
-    public InventoryItemSO Selected;
-    public Inventory inventory;
+    public Vector3 ammoButton = new Vector3(-390, -100, 0f);
+    public Vector3 panelButton = new Vector3(-240, -100, 0f);
+    public Vector3 partButton = new Vector3(-90, -100, 0f);
+    private Queue<InventoryItemSO> itemsToCraft = new Queue<InventoryItemSO>();
+    private Inventory inventory;
     public Dictionary<GameObject, InventoryItemSO> craftableItems;
     // Start is called before the first frame update
     void Start()
@@ -27,19 +29,19 @@ public class ProcessorCrafting : MonoBehaviour
         //canvas = GameObject.FindGameObjectWithTag("ProcessorCanvas");
         var ammo = Instantiate(Ammo.Sprite, Vector3.zero, Quaternion.identity, canvas.transform);
         Destroy(ammo.transform.GetChild(0).gameObject);
-        ammo.GetComponent<RectTransform>().localPosition = new Vector3(-390, -100, 0f);
+        ammo.GetComponent<RectTransform>().localPosition = ammoButton;
         AddEvent(ammo, EventTriggerType.PointerClick, delegate { OnClick(ammo); });
         craftableItems.Add(ammo, Ammo);
 
         var panel = Instantiate(Panel.Sprite, Vector3.zero, Quaternion.identity, canvas.transform);
         Destroy(panel.transform.GetChild(0).gameObject);
-        panel.GetComponent<RectTransform>().localPosition = new Vector3(-240, -100, 0f);
+        panel.GetComponent<RectTransform>().localPosition = panelButton;
         AddEvent(panel, EventTriggerType.PointerClick, delegate { OnClick(panel); });
         craftableItems.Add(panel, Panel);
 
         var machinePart = Instantiate(MachinePart.Sprite, Vector3.zero, Quaternion.identity, canvas.transform);
         Destroy(machinePart.transform.GetChild(0).gameObject);
-        machinePart.GetComponent<RectTransform>().localPosition = new Vector3(-90, -100, 0f);
+        machinePart.GetComponent<RectTransform>().localPosition = partButton;
         AddEvent(machinePart, EventTriggerType.PointerClick, delegate { OnClick(machinePart); });
         craftableItems.Add(machinePart, MachinePart);
 
@@ -56,28 +58,35 @@ public class ProcessorCrafting : MonoBehaviour
 
     public void OnClick(GameObject obj)
     {
-        Selected = craftableItems[obj];
+        if (inventory.inventory[0].ItemCount > itemsToCraft.Count)
+        {
+            itemsToCraft.Enqueue(craftableItems[obj]);
+        }
+        else
+        {
+            //Error message? May also need to let them remove from queue
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Selected != null && inventory.inventory[0].ItemCount > 0)
+        if (itemsToCraft.Count > 0 && inventory.inventory[0].ItemCount > 0)
         {
             progressBar.value = 1f - craftTimer / craftInterval;
             if (craftTimer <= 0)
             {
                 craftTimer = craftInterval;
-                CraftItem();
+                CraftItem(itemsToCraft.Dequeue());
             }
             craftTimer -= Time.deltaTime;
         }
     }
 
-    public void CraftItem()
+    public void CraftItem(InventoryItemSO item)
     {
         inventory.RemoveItem(inventory.inventory[0]);
-        inventory.AddItem(Selected);
+        inventory.AddItem(item);
         progressBar.value = 0;
     }
 }
