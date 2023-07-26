9facf075a4718e5c3c15b74fcb9dc9d9be96b90d
Silvano Gallegos <81341115+PseudoSaurus@users.noreply.github.com>
2023-07-25 19:00:31 -0500

    Scripting UI

---
+++ Assets/Game Assets/Scripts/Equipment/LoadoutUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Events.EquipmentSwapping.cs;
+
+public class LoadoutUI : MonoBehaviour
+{
+    private EquipmentSwapping equipment;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
