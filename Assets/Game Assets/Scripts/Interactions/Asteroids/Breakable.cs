df9d690d87279303f980f3f39de15399575a99dc
MusicalMoon <sakinam831@gmail.com>
2023-08-03 15:44:49 -0500

    Changed Breakable Script
    Added to the breakable Script, created a prefab of the small broken asteroids and made it to spawn when the unbroken asteroid dies.

--- Assets/Game Assets/Scripts/Interactions/Asteroids/Breakable.cs
+++ Assets/Game Assets/Scripts/Interactions/Asteroids/Breakable.cs
@@ -2,21 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Breakable : MonoBehaviour {
+public class Breakable : MonoBehaviour
+{
 
-    [SerializeField] private GameObject _replacement;
-    [SerializeField] private float _breakForce = 2;
+    [SerializeField] private GameObject _replacement; //broken asteroid
+    /*[SerializeField] private GameObject _original; //original asteroid*/
     [SerializeField] private float _collisionMultiplier = 100;
-    [SerializeField] private bool _broken;
-    [SerializeField] private int relativeVelocity;
+    [SerializeField] private int time = 5; //time taken to despawn
+
+    //On awake the broken asteroid will replace the original one and a force will be applied to cause it to crumble
+    // A smoke effect will also be applied at the same time
+    // After a set period of time it will despawn
     void Awake()
     {
-        /*var replacement = Instantiate(_replacement, transform.position, transform.rotation);
-
-        private var rbs = replacement.GetComponentInChildren<Rigidbody>();
-        foreach(var rb in rbs)
-        {
-            rb.AddExplosionForce(relativeVelocity * _collisionMultiplier, 1, 2);
-        }*/
     }
+
+    public void AlmostAwake()
+    {
+        //get position of drill, so that's where the force comes from
+        Vector3 mousePos = Input.mousePosition;
+
+        //spawns replacement in place of original
+        var replacement = Instantiate(_replacement, null);
+        replacement.transform.position = transform.position;
+        replacement.transform.rotation = transform.rotation;
+
+        //calls each child
+        var rbs = replacement.GetComponentsInChildren<Rigidbody>();
+        //adds force to each child
+        foreach (var rb in rbs)
+        {
+            //rb.transform.SetParent(null);
+            rb.AddExplosionForce(5 * _collisionMultiplier, mousePos, 2);
+            //Delete object sequence
+          //  Destroy(rb.gameObject, time);
+        }
+    }
+
 }
