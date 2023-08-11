82328052d647ba9715314c5c63233f8e4d1b62b1
Eric WIlhoit <a10rec@myfastmail.com>
2023-08-11 06:51:39 -0500

    Removed Unused Namespaces

--- Assets/Game Assets/Scripts/Agent/Krill/AIKrillFollowState.cs
+++ Assets/Game Assets/Scripts/Agent/Krill/AIKrillFollowState.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityMovementAI;
--- Assets/Game Assets/Scripts/Inventory/DisplayCrafting.cs
+++ Assets/Game Assets/Scripts/Inventory/DisplayCrafting.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using static UnityEditor.Progress;
 using UnityEngine.UI;
 using static UnityEngine.Rendering.DebugUI;
 using UnityEngine.EventSystems;
