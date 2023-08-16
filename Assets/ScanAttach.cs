9ed76c0f3f800ef9b89050df58cbb6ab37233c52
Daniel <dancoches@gmail.com>
2023-08-16 14:16:18 -0500

    Got scanning to attach to world

--- Assets/ScanAttach.cs
+++ Assets/ScanAttach.cs
@@ -10,12 +10,14 @@ public class ScanAttach : MonoBehaviour
     [SerializeField] protected Transform rayPoint;
     public VisualEffect visFX;
     public Vector3 vectorToModify;
-    public Transform originalTransform;
+    private float goalZ, currentZ;
+
+    [SerializeField] protected float ZMultiplier, snapSpeed, goalZOffset;
 
     private void Awake()
     {
         vectorToModify = visFX.GetVector3("Projectile Line Offset");
-        originalTransform = planeTransform;
+        currentZ = goalZ = 5;
     }
     private void Update()
     {
@@ -24,28 +26,41 @@ public class ScanAttach : MonoBehaviour
 
     private void AttachToNearestSurface()
     {
-        Ray ray = new Ray(rayPoint.position, -originPoint.forward); // Cast the ray in the forward direction of the originPoint
+        Ray ray = new Ray(rayPoint.position, originPoint.forward); // Cast the ray in the forward direction of the originPoint
         Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red);
 
 
         RaycastHit hitInfo;
         if (Physics.Raycast(ray, out hitInfo, 100f , LayerMask.GetMask("Default")))
         {
-            planeTransform.position = hitInfo.point;
+            //planeTransform.position = hitInfo.point;
+
+            goalZ = originPoint.InverseTransformPoint(hitInfo.point).z + goalZOffset;
+
+            //var pos = planeTransform.localPosition;
+            //pos.x = pos.y = 0;
+            //planeTransform.localPosition = pos;
 
             // Calculate the rotation to align the plane's up with the hit surface normal
-            Quaternion rotation = Quaternion.FromToRotation(planeTransform.up, hitInfo.normal) * planeTransform.rotation;
-            planeTransform.rotation = rotation;
+            //Quaternion rotation = Quaternion.FromToRotation(planeTransform.up, hitInfo.normal) * planeTransform.rotation;
+            //planeTransform.rotation = rotation;
 
-            Vector3 distanceDifference = planeTransform.position - originPoint.position;
+            //Vector3 distanceDifference = planeTransform.position - originPoint.position;
 
             
-            visFX.SetVector3("Projectile Line Offset", new Vector3(5, 5, vectorToModify.z - distanceDifference.z));
+            //visFX.SetVector3("Projectile Line Offset", new Vector3(5, 5, vectorToModify.z - (planeTransform.localPosition.z * ZMultiplier)));
         } else
         {
-            visFX.SetVector3("Projectile Line Offset", new Vector3(5,5,5));
-            planeTransform = originalTransform;
+            Debug.Log("No hit");
+            goalZ = 0;
+            //visFX.SetVector3("Projectile Line Offset", new Vector3(5,5,5));
+            //planeTransform.localPosition = Vector3.zero;
         }
+
+        currentZ = Mathf.Lerp(currentZ, goalZ, Time.deltaTime * snapSpeed);
+        visFX.SetVector3("Projectile Line Offset", new Vector3(5, 5, vectorToModify.z - currentZ * ZMultiplier));
+        planeTransform.localPosition = new Vector3(0, 0, -currentZ);
+
     }
 
     //private void FixedUpdate()
