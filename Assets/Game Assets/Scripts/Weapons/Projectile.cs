0460145347c57631a72bf88719e6cf6262d1a398
Eric WIlhoit <a10rec@myfastmail.com>
2023-08-28 05:46:44 -0500

    Bullet fixes

--- Assets/Game Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Game Assets/Scripts/Weapons/Projectile.cs
@@ -10,7 +10,7 @@ public class Projectile : MonoBehaviour
     [SerializeField] float _speed;
     [SerializeField] float _lifetime = 5f;
 
-    private BasicHitResponder _hitResponder;
+    private DestroyHitResponder _hitResponder;
     private int _damage;
     private Rigidbody _rbody;
     private Transform _origin;
@@ -19,7 +19,7 @@ public class Projectile : MonoBehaviour
     public void Configure(Transform originTransform, int damage)
     {
         //configure
-        _hitResponder = _hitbox.GetComponent<BasicHitResponder>();
+        _hitResponder = _hitbox.GetComponent<DestroyHitResponder>();
         _origin = originTransform;
         //_damage = damage;
 
